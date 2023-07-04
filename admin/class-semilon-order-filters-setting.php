44e2e797a0fad771bd6f835604423b314d175b3a
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-04 18:11:06 +0330

    feet (admin/class-semilon-order-filters-setting.php): start create tab

--- admin/class-semilon-order-filters-setting.php
+++ admin/class-semilon-order-filters-setting.php
@@ -10,6 +10,13 @@ if (!class_exists('Semilon_Order_Filters_Setting')) {
         public function __construct()
         {
             $this->id = SEMILON_ORDER_FILTERS_ID;
+
+            $this->current_tab = ( isset( $_GET[ 'tab' ] ) ) ? $_GET[ 'tab' ] : 'general';
+
+            // Tab under WooCommerce settings
+            $this->settings_tabs = array(
+                $this->id => 'Order Filters'
+            );
         }
     }
 }
