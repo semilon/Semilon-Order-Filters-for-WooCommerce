494f48b9f24de30083494b5d3f6a7d9369996223
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-04 14:49:13 +0330

    feet (main.php): create file and set important plugin comments

---
+++ main.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @link            https://Semilon.dev/
+ * @since           1.0.0
+ * @package         Pass_WooCommerce_Shipping
+ *
+ * @wordpress-plugin
+ * Plugin Name:     Semilon Order Filters for WooCommerce
+ * Plugin URI:      https://github.com/semilon/semilon-order-filters-for-woocommerce
+ * Description:     all order filters for woocommerce
+ * Version:         1.0.0
+ * Author:          Semilon
+ * Author:          Mostafa Sharami
+ * Author:          Majid Vahidkhoo
+ * Author URI:      https://Semilon.dev/
+ * Author URI:      http://MostafaSharami.com/
+ * Author URI:      http://Majva.com/
+ *
+ * Copyright:       © 2023 https://Semilon.dev/.
+ * License:         MIT License
+ */
