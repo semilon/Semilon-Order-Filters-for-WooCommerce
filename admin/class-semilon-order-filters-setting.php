d6f9dd3c272604717646ef4ac1155e65053c2083
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-04 18:12:15 +0330

    feet (admin/class-semilon-order-filters-setting@action_links): create method and use by add_filter()

--- admin/class-semilon-order-filters-setting.php
+++ admin/class-semilon-order-filters-setting.php
@@ -17,6 +17,30 @@ if (!class_exists('Semilon_Order_Filters_Setting')) {
             $this->settings_tabs = array(
                 $this->id => 'Order Filters'
             );
+
+            add_filter( 'plugin_action_links_' . $this->id, array( $this, 'action_links' ) );
+        }
+
+        /**
+         * Add action links under WordPress > Plugins
+         *
+         * @param $links
+         * @return array
+         */
+        public function action_links( $links ) {
+
+            $settings_slug = 'woocommerce';
+
+            if ( version_compare( WOOCOMMERCE_VERSION, "2.1.0" ) >= 0 ) {
+
+                $settings_slug = 'wc-settings';
+            }
+
+            $plugin_links = array(
+                '<a href="' . admin_url( 'admin.php?page=' . $settings_slug . '&tab=' . $this->id ) . '">' . __( 'Settings', 'woocommerce' ) . '</a>',
+            );
+
+            return array_merge( $plugin_links, $links );
         }
     }
 }
