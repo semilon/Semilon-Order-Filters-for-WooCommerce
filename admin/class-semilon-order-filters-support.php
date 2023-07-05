e926f743a60ad95725ae2a23abc3b304c7615f67
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-05 18:18:47 +0330

    debug (admin/class-semilon-order-filters-support@support_template): create method

--- admin/class-semilon-order-filters-support.php
+++ admin/class-semilon-order-filters-support.php
@@ -12,6 +12,10 @@ if (!class_exists('Semilon_Order_Filters_Support')) {
             add_filter( 'plugin_action_links_' . SEMILON_ORDER_FILTERS_PLUGIN_BASENAME, array( $this, 'action_links' ) );
         }
 
+        public function support_template() {
+            echo '<br /><br /><br /><center><h1 style="color: green">WoW!!!</h1><h2 style="color: red">Semilon support page</h2></center>';
+        }
+
         /**
          * Add action links under WordPress > Plugins
          *
