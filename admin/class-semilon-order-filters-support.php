688809d8a5a5e523d8fd1a24ec8d6d160efa66d2
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-05 18:16:55 +0330

    debug (admin/class-semilon-order-filters-support.php): create class

---
+++ admin/class-semilon-order-filters-support.php
@@ -0,0 +1,13 @@
+<?php
+
+/*if (!SEMILON_ORDER_FILTERS_IS_ACTIVE)
+    return;*/
+
+
+if (!class_exists('Semilon_Order_Filters_Support')) {
+    class Semilon_Order_Filters_Support
+    {
+    }
+}
+
+
