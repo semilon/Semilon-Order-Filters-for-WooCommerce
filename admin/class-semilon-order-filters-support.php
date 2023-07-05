b9244248d3098c37f686b3ad02e51d48f1d1c5c9
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-05 18:18:34 +0330

    debug (admin/class-semilon-order-filters-support@action_links): create method for add 'support' item in plugins page

--- admin/class-semilon-order-filters-support.php
+++ admin/class-semilon-order-filters-support.php
@@ -7,6 +7,25 @@
 if (!class_exists('Semilon_Order_Filters_Support')) {
     class Semilon_Order_Filters_Support
     {
+        public function __construct()
+        {
+            add_filter( 'plugin_action_links_' . SEMILON_ORDER_FILTERS_PLUGIN_BASENAME, array( $this, 'action_links' ) );
+        }
+
+        /**
+         * Add action links under WordPress > Plugins
+         *
+         * @param $links
+         * @return array
+         */
+        public function action_links( $links ) {
+            $plugin_links[] = '<a href="'
+                . admin_url( 'admin.php?page=semilon-support' ) . '">'
+                . __('Support', SEMILON_ORDER_FILTERS_TRANSLATE_ID) . '</a>';
+
+            return array_merge( $plugin_links, $links );
+
+        }
     }
 }
 
