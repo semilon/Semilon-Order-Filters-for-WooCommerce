fcd4bc0630dbbb6e6464ae623d81cc7f93e05aaf
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-08 17:41:32 +0330

    feat (admin/class-semilon-order-filters-manager@list): create method

--- admin/class-semilon-order-filters-manager.php
+++ admin/class-semilon-order-filters-manager.php
@@ -13,6 +13,34 @@ if (!class_exists('Semilon_Order_Filters_Manager')) {
         {
         }
 
+        private function list($exceptions = array())
+        {
+            $files = scandir($this->base);
+            unset($files[0]);
+            unset($files[1]);
+
+            $list = array();
+
+            GLOBAL $Semilon_order_filters_fields;
+
+            foreach ($files as $index => $file) {
+                if (is_file($this->base . DIRECTORY_SEPARATOR . $file) && !in_array($file, $exceptions)) {
+                    $filter = $this->fetch_filter_name($file);
+                    $object = $this->fetch_class_name($file);
+                    /*$list[] = [
+                        'file' => $file,
+                        'filter'=> $filter,
+                        'class' => $object,
+                    ];*/
+
+                    $options = get_option(SEMILON_ORDER_FILTERS_ID . '_' . $filter) === 'yes';
+                    require_once($this->base . DIRECTORY_SEPARATOR . $file);
+                    $filter = new $object($options);
+                    $Semilon_order_filters_fields[] = $filter->field;
+                }
+            }
+        }
+
         private function fetch_filter_name($file)
         {
             $filter = preg_replace('/class-semilon-order-filters-([a-z\-]+).php/i', '${1}', $file);
