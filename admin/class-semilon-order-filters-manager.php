bf91056b95993a68b5832ef7109b2a0123a8acc7
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-08 11:48:09 +0330

    feat (admin/class-semilon-order-filters-manager.php): create class

---
+++ admin/class-semilon-order-filters-manager.php
@@ -0,0 +1,16 @@
+<?php
+
+/*if (!SEMILON_ORDER_FILTERS_IS_ACTIVE)
+    return;*/
+
+
+if (!class_exists('Semilon_Order_Filters_Manager')) {
+    class Semilon_Order_Filters_Manager
+    {
+        public function __construct()
+        {
+        }
+    }
+}
+
+
