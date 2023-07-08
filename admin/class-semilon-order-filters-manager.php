698fb2dfbc8f4fc2dcf693a3c4e465f0c40cc104
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-08 17:39:23 +0330

    feat (admin/class-semilon-order-filters-manager@fetch_class_name): create method

--- admin/class-semilon-order-filters-manager.php
+++ admin/class-semilon-order-filters-manager.php
@@ -10,6 +10,15 @@ if (!class_exists('Semilon_Order_Filters_Manager')) {
         public function __construct()
         {
         }
+
+        private function fetch_class_name($file)
+        {
+            $class = preg_replace('/class-semilon-order-filters-([a-z\-]+).php/i', '${1}', $file);
+            $class = implode(' ', explode('-', $class));
+            $class = ucwords($class);
+            $class = implode('_', explode(' ', $class));
+            return 'Semilon_Order_Filters_' . $class;
+        }
     }
 }
 
