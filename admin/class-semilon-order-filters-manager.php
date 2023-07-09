fbf1fcfabd8c7a9c4da71d17f8440910b24547a6
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-09 13:00:29 +0330

    feat (admin/class-semilon-order-filters-manager@load_filter): create method

--- admin/class-semilon-order-filters-manager.php
+++ admin/class-semilon-order-filters-manager.php
@@ -56,6 +56,15 @@ if (!class_exists('Semilon_Order_Filters_Manager')) {
             $class = implode('_', explode(' ', $class));
             return 'Semilon_Order_Filters_' . $class;
         }
+
+        private function load_filter($file) {
+            $filter = $this->fetch_filter_name($file);
+            $object = $this->fetch_class_name($file);
+
+            $active = get_option(SEMILON_ORDER_FILTERS_ID . '_' . $filter) === 'yes';
+            require_once($this->base . DIRECTORY_SEPARATOR . $file);
+            return (new $object($active))->field;
+        }
     }
 }
 
