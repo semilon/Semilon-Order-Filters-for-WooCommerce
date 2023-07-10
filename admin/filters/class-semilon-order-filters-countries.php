03620cbaa905d12705d6091a3d952e413b595a67
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-10 18:23:30 +0330

    feat (admin/filters/class-semilon-order-filters-countries.php): create 'Countries' filter class with base options

---
+++ admin/filters/class-semilon-order-filters-countries.php
@@ -0,0 +1,29 @@
+<?php
+
+/*if (!SEMILON_ORDER_FILTERS_IS_ACTIVE)
+    return;*/
+
+
+if (!class_exists('Semilon_Order_Filters_Countries')) {
+    class Semilon_Order_Filters_Countries extends Semilon_Order_Filters_Main
+    {
+        protected $name = 'country';
+        protected $colection = 'countries';
+        protected $item_tags = array('billing_country' => '_billing_country');
+
+        public function __construct($isActive)
+        {
+            $this->field = array(
+                'name'	  => __('Countries', SEMILON_ORDER_FILTERS_TRANSLATE_ID),
+                'desc'    => __('Filter countries buy your products.', SEMILON_ORDER_FILTERS_TRANSLATE_ID),
+                'id'	  => SEMILON_ORDER_FILTERS_ID . '_countries',
+                'type'	  => 'checkbox',
+                'default' => 'yse'
+            );
+
+            parent::__construct($isActive);
+        }
+    }
+}
+
+
