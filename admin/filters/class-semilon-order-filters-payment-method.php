3ff0d807f9a13c75ea2ba52ec6333df4f5f1e8bf
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-11 12:28:17 +0330

    feat (admin/filters/class-semilon-order-filters-payment-method@$item_tags): create variable

--- admin/filters/class-semilon-order-filters-payment-method.php
+++ admin/filters/class-semilon-order-filters-payment-method.php
@@ -9,5 +9,9 @@ if (!class_exists('Semilon_Order_Filters_Payment_Method')) {
     {
         protected $name = 'payment_method';
         protected $colection = 'payment_methods';
+        protected $item_tags = array(
+            'payment_method'       => '_payment_method',
+            'payment_method_title' => '_payment_method_title'
+        );
     }
 }
