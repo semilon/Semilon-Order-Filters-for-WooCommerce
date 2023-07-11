3008affb358c033eddb55c387a992ff5f745cbcb
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-11 17:39:39 +0330

    feat (admin/filters/class-semilon-order-filters-billing-first-names.php): create file & class

---
+++ admin/filters/class-semilon-order-filters-billing-first-names.php
@@ -0,0 +1,11 @@
+<?php
+
+/*if (!SEMILON_ORDER_FILTERS_IS_ACTIVE)
+    return;*/
+
+
+if (!class_exists('Semilon_Order_Filters_Billing_First_Names')) {
+    class Semilon_Order_Filters_Billing_First_Names extends Semilon_Order_Filters_Main
+    {
+    }
+}
