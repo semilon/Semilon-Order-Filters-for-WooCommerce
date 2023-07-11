ef71fcf3520586ed97042b749c18c2b08f4cf5f1
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-11 12:28:02 +0330

    feat (admin/filters/class-semilon-order-filters-payment-method@$name): create variable

--- admin/filters/class-semilon-order-filters-payment-method.php
+++ admin/filters/class-semilon-order-filters-payment-method.php
@@ -7,5 +7,6 @@
 if (!class_exists('Semilon_Order_Filters_Payment_Method')) {
     class Semilon_Order_Filters_Payment_Method extends Semilon_Order_Filters_Main
     {
+        protected $name = 'payment_method';
     }
 }
