7d443b605dda7b2296eb0cf56bff72af5ef2a23c
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-11 12:27:43 +0330

    feat (admin/filters/class-semilon-order-filters-payment-method.php): create class

---
+++ admin/filters/class-semilon-order-filters-payment-method.php
@@ -0,0 +1,11 @@
+<?php
+
+/*if (!SEMILON_ORDER_FILTERS_IS_ACTIVE)
+    return;*/
+
+
+if (!class_exists('Semilon_Order_Filters_Payment_Method')) {
+    class Semilon_Order_Filters_Payment_Method extends Semilon_Order_Filters_Main
+    {
+    }
+}
