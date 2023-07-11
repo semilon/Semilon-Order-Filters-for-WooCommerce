c2d8a31c4152ebe12a167c86afe4a834e8f40a72
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-11 14:02:16 +0330

    fix (admin/filters/class-semilon-order-filters-payment-methods): remove $collection

--- admin/filters/class-semilon-order-filters-payment-methods.php
+++ admin/filters/class-semilon-order-filters-payment-methods.php
@@ -8,7 +8,6 @@ if (!class_exists('Semilon_Order_Filters_Payment_Methods')) {
     class Semilon_Order_Filters_Payment_Methods extends Semilon_Order_Filters_Main
     {
         protected $name = 'payment_method';
-        protected $colection = 'payment_methods';
         protected $item_tags = array(
             'payment_method'       => '_payment_method',
             'payment_method_title' => '_payment_method_title'
