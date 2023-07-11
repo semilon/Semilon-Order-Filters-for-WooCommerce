cd5840f884cf1fbaaf23ed839ffb9ea3d1f55153
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-11 15:34:00 +0330

    fix (admin/filters/class-semilon-order-filters-user-email.php): create file & class

---
+++ admin/filters/class-semilon-order-filters-user-email.php
@@ -0,0 +1,11 @@
+<?php
+
+/*if (!SEMILON_ORDER_FILTERS_IS_ACTIVE)
+    return;*/
+
+
+if (!class_exists('Semilon_Order_Filters_User_Email')) {
+    class Semilon_Order_Filters_User_Email extends Semilon_Order_Filters_Main
+    {
+    }
+}
