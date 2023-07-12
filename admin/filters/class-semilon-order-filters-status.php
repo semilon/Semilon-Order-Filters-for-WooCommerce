401b9173502322ca0f129e13d96b5e5d42fb303d
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-12 12:33:07 +0330

    feat (admin/filters/class-semilon-order-filters-status.php): create file & class

---
+++ admin/filters/class-semilon-order-filters-status.php
@@ -0,0 +1,11 @@
+<?php
+
+/*if (!SEMILON_ORDER_FILTERS_IS_ACTIVE)
+    return;*/
+
+
+if (!class_exists('Semilon_Order_Filters_Status')) {
+    class Semilon_Order_Filters_Status extends Semilon_Order_Filters_Main
+    {
+    }
+}
