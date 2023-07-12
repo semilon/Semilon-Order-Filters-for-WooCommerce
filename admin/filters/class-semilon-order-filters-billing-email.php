c69caec7463bf4c133491efd7d8f336440fd73f0
Mostafa Sharami <Mostafa.Sharami@gmail.com>
2023-07-12 18:14:50 +0330

    feat (admin/filters/class-semilon-order-filters-billing-email.php): change name

--- admin/filters/class-semilon-order-filters-billing-email.php
+++ admin/filters/class-semilon-order-filters-billing-email.php
@@ -4,12 +4,12 @@
     return;*/
 
 
-if (!class_exists('Semilon_Order_Filters_User_Email')) {
-    class Semilon_Order_Filters_User_Email extends Semilon_Order_Filters_Main
+if (!class_exists('Semilon_Order_Filters_Billing_Email')) {
+    class Semilon_Order_Filters_Billing_Email extends Semilon_Order_Filters_Main
     {
         protected $name = 'user_email';
         protected $joins = array(array(
-            'name' => 'user_email',
+            'name' => 'billing_email',
             'value'=> '_billing_email'
         ));
     }
